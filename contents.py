9f0c2f55410f7eee113dbe40dab9ec501fad50ee
Damien Allen <dev@dallen.co>
2024-03-27 23:19:02 +0100

    Pydantic model for CV contents

---
+++ contents.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import yaml
+from pydantic import BaseModel
+
+
+class BioSection(BaseModel):
+    name: str
+    location: str
+    nationality: str
+
+
+class ContactSection(BaseModel):
+    email: str
+    phone: str
+
+
+class LanguageItem(BaseModel):
+    name: str
+    level: str
+
+
+class EducationItem(BaseModel):
+    institution: str
+    degree: str
+    years: str
+
+
+class WorkItem(BaseModel):
+    company: str
+    title: str
+    years: str
+    description: str
+
+
+class SkillListItem(BaseModel):
+    label: str
+    items: list[str]
+
+
+class CVContents(BaseModel):
+    bio: BioSection
+    contact: ContactSection
+    language: list[LanguageItem]
+    education: list[EducationItem]
+    work: list[WorkItem]
+    skills: list[SkillListItem]
+
+
+def load_contents(contents_yaml_path: Path) -> CVContents:
+    with open(contents_yaml_path) as f:
+        return CVContents.model_validate(yaml.safe_load(f))
